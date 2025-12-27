c52561b5a31524daa00f018f3a7fcdbe564e0507
bal16 <ahmadhamidbalya@gmail.com>
2025-12-28 06:54:22 +0700

    ui: settings tab

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,17 +1,38 @@
+import { createSettingsStyles } from "@/assets/styles/settings.styles";
+import DangerZone from "@/components/settings/DangerZone";
+import Header from "@/components/settings/Header";
+import Preferences from "@/components/settings/Preferences";
+import ProgressStats from "@/components/settings/ProgressStats";
 import useTheme from "@/hooks/useTheme";
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-const Settings = () => {
-  const { toggleDarkMode } = useTheme();
+import { ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SettingsScreen = () => {
+  const { colors } = useTheme();
+
+  const settingsStyles = createSettingsStyles(colors);
+
   return (
-    <View>
-      <Text>settings page</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Toggle the mode</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingsStyles.container}
+    >
+      <SafeAreaView style={settingsStyles.safeArea}>
+        <Header />
+
+        <ScrollView
+          style={settingsStyles.scrollView}
+          contentContainerStyle={settingsStyles.content}
+          showsVerticalScrollIndicator={false}
+        >
+          <ProgressStats />
+          <Preferences />
+          <DangerZone />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
-
-export default Settings;
+export default SettingsScreen;
