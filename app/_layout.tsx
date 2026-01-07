60abd018e0ffaaafccaf1ba24ac316893e1a8fc6
bal16 <ahmadhamidbalya@gmail.com>
2026-01-07 19:32:01 +0700

    feat: integrate migrations for database management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
+import migrations from "@/drizzle/migrations";
 import { ThemeProvider } from "@/hooks/useTheme";
 import { TodosProvider } from "@/hooks/useTodos";
+import { db } from "@/lib/db/client";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 // import useInit from "@/lib/db/use-init";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   // useInit();
+  useMigrations(db, migrations);
   return (
     <ThemeProvider>
       <TodosProvider>
